1b80a38c3a7966bfe28ff7d5e19c996b615a406f
Robin Marchel <git@rmarchel.de>
2020-01-29 18:09:44 +0100

    Add setup.py to use it as a lib

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='IDNNS',
+      version='0.1',
+      description='IDNNS',
+      author='Rob',
+      author_email='rob@example.com',
+      url='https://',
+      packages=['idnns','idnns.information','idnns.networks','idnns.plots'],
+     )
+
